0472c11b4a9d73c6c7d536e9f0b3bf3ca257aba5
fbarrett12 <f.barrett12@gmail.com>
2019-12-20 22:19:01 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+
+// function jadenCase(str){
+//   casedArr = []
+//   str = this.split(" ")
+//   for(i=0; i < str.length; i++){
+//     if(str[i][0] === str[i][0].toUpperCase()){
+//       casedArr.push(str[i])
+//     } else {
+//       capFirst = str[i].slice(0,1).toUpperCase()
+//       restOfWord = str[i].slice(1)
+
+//       casedArr.push(capFirst + restOfWord)
+//     }
+//   }
+//   return casedArr.join(" ")
+// }
+
+// jadenCase("The rain in Spain")
+
+function jadenCaseTwo(str){
+  str = str.split(" ")
+  newArr = str.map(x => x[0].toUpperCase() + x.slice(1))
+  return newArr.join(" ")
+}
+
+jadenCaseTwo("The rain in Spain one more word")
